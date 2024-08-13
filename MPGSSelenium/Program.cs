6131582e168630d6239a57e3b109c754953726e3
muhammad Waqar <muhammadwaqar552@gmail.com>
2024-08-13 17:04:23 +0400

    Copied

--- MPGSSelenium/Program.cs
+++ MPGSSelenium/Program.cs
@@ -3,25 +3,28 @@ using OpenQA.Selenium;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using OpenQA.Selenium.Firefox;
 using System.Threading;
+using System.IO;
 
 namespace MPGSSelenium
 {
     public class Program
     {
+        public static IWebDriver driver = null;
+        public static string midfile = @"C:\Users\muham\Desktop\Projects\MIDLIST.csv";
         public static void Main(string[] args)
         {
             Console.WriteLine("Program start...");
-            using (IWebDriver driver = new ChromeDriver())
+            List<string> midList = File.ReadAllLines(midfile).Skip(1).Select(v => MidFromCsv(v)).ToList();
+            var options = new ChromeOptions();
+            options.AddArgument("no-sandbox");
+            using (driver = new ChromeDriver(options))
             {
                 try
                 {
+                    driver.Manage().Window.Maximize();
                     driver.Manage().Timeouts().PageLoad = TimeSpan.FromMinutes(5);
                     driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromMinutes(5);
-                    driver.Manage().Window.Maximize();
                     driver.Navigate().GoToUrl("https://adcb.gateway.mastercard.com/mm/");
 
                     IWebElement clientIdTextBox = driver.FindElement(By.Id("ownerId"));
@@ -37,56 +40,83 @@ namespace MPGSSelenium
                     passwordTextBox.SendKeys("");
                     passwordTextBox.SendKeys(Keys.Enter);
 
-                    Thread.Sleep(3000);
+                    Thread.Sleep(5000);
 
-                    driver.Navigate().GoToUrl("https://adcb.gateway.mastercard.com/mm/merchantSearch.s?selectedMenuItem=merchantSearch");
+                    //driver.Navigate().GoToUrl("https://adcb.gateway.mastercard.com/mm/createNewMerchant.s?selectedMenuItem=createMerchant");
 
-                    IWebElement merchantIdTextBox = driver.FindElement(By.Id("merchantId"));
-                    merchantIdTextBox.Clear();
-                    merchantIdTextBox.SendKeys("");
-                    merchantIdTextBox.SendKeys(Keys.Enter);
+                    //Thread.Sleep(200);
+                    //((IJavaScriptExecutor)driver).ExecuteScript("window.scrollTo(0, document.body.scrollHeight - 100)");
+                    //Thread.Sleep(200);
 
-                    Thread.Sleep(3000);
+                    //driver.FindElement(By.Id("password.row")).Click();
+                    //Thread.Sleep(50);
 
-                    IList<IWebElement> links = driver.FindElements(By.TagName("a"));
-                    foreach (var link in links)
+                    //driver.FindElement(By.ClassName("show-password")).Click(); 
+                    //Thread.Sleep(50);
+                    //IWebElement element = driver.FindElement(By.Id("password"));
+                    //element.SendKeys("1");
+
+                    foreach (string mid in midList)
                     {
-                        string href = link.GetAttribute("href");
-                        if (href.Contains("mm/paymentDetails.s?merchantSystemId"))
+                        Thread.Sleep(3000);
+
+                        driver.Navigate().GoToUrl("https://adcb.gateway.mastercard.com/mm/merchantSearch.s?selectedMenuItem=merchantSearch");
+
+                        IWebElement merchantIdTextBox = driver.FindElement(By.Id("merchantId"));
+                        merchantIdTextBox.Clear();
+                        merchantIdTextBox.SendKeys(mid);
+                        merchantIdTextBox.SendKeys(Keys.Enter);
+
+                        Thread.Sleep(3000);
+
+                        //IList<IWebElement> links = driver.FindElements(By.TagName("a"));
+                        //foreach (var link in links)
+                        //{
+                        //    string href = link.GetAttribute("href");
+                        //    if (href.Contains("mm/paymentDetails.s?merchantSystemId"))
+                        //    {
+                        //        link.Click();
+                        //        break;
+                        //    }
+                        //}
+                        //Thread.Sleep(3000);
+
+                        //((IJavaScriptExecutor)driver).ExecuteScript("window.scrollTo(0, document.body.scrollHeight - 100)");
+
+                        //Thread.Sleep(200);
+
+                        //IWebElement chkoptout = driver.FindElement(By.Id("mciSpProductsOptOut"));
+                        //chkoptout.Click();
+
+                        //IWebElement submitbtn = driver.FindElement(By.ClassName("SubmitSubmit"));
+                        //submitbtn.Click();
+
+                        //Thread.Sleep(2000);
+
+                        ((IJavaScriptExecutor)driver).ExecuteScript("window.scrollTo(0, document.body.scrollHeight - 100)");
+
+                        Thread.Sleep(200);
+
+                        IWebElement linkApprove = driver.FindElement(By.Id("merchantApprove"));
+                        linkApprove.Click();
+
+                        Thread.Sleep(2000);
+
+                        ((IJavaScriptExecutor)driver).ExecuteScript("window.scrollTo(0, document.body.scrollHeight - 100)");
+
+                        Console.WriteLine("Press y to approve merchant");
+                        string action = Console.ReadLine();
+                        if (action == "y")
                         {
-                            link.Click();
-                            break;
+                            IWebElement btnapproved = driver.FindElement(By.ClassName("SubmitSubmit"));
+                            btnapproved.Click();
+                        }
+                        else
+                        {
+                            Console.WriteLine("Pls manually check and approve MID: "+ mid);
                         }
                     }
-                    Thread.Sleep(3000);
-
-                    ((IJavaScriptExecutor)driver).ExecuteScript("window.scrollTo(0, document.body.scrollHeight - 100)");
-
-                    Thread.Sleep(200);
-
-                    IWebElement chkoptout = driver.FindElement(By.Id("mciSpProductsOptOut"));
-                    chkoptout.Click();
-
-                    IWebElement submitbtn = driver.FindElement(By.ClassName("SubmitSubmit"));
-                    submitbtn.Click();
-
-                    Thread.Sleep(2000);
-
-                    ((IJavaScriptExecutor)driver).ExecuteScript("window.scrollTo(0, document.body.scrollHeight - 100)");
-
-                    Thread.Sleep(200);
-
-                    IWebElement linkApprove = driver.FindElement(By.Id("merchantApprove"));
-                    linkApprove.Click();
-
-                    Thread.Sleep(4000);
-
-                    ((IJavaScriptExecutor)driver).ExecuteScript("window.scrollTo(0, document.body.scrollHeight - 100)");
-
-                    IWebElement btnapproved = driver.FindElement(By.ClassName("SubmitSubmit"));
-                    btnapproved.Click();
                     
-
                     Console.WriteLine("Program end...");
                     Console.ReadLine();
                 }
@@ -99,6 +129,10 @@ namespace MPGSSelenium
                     driver.Quit();
                 }
             }
+        }
+        public static string MidFromCsv(string csvLine)
+        {
+            return csvLine.Split(',')[0];
         }
     }
 }
